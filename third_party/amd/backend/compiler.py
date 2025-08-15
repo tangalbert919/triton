b42b6f3cd020f251c4e49ab1321a3b53ce92331c
Seunghoon Lee <lshqqytiger@naver.com>
2025-08-15 21:11:52 +0900

    fix

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -416,13 +416,17 @@ class HIPBackend(BaseBackend):
         if knobs.compilation.enable_asan:
             target_features = '+xnack'
         hsaco = amd.assemble_amdgcn(src, options.arch, target_features)
-        with tempfile.NamedTemporaryFile() as tmp_out:
-            with tempfile.NamedTemporaryFile() as tmp_in:
-                with open(tmp_in.name, "wb") as fd_in:
-                    fd_in.write(hsaco)
-                amd.link_hsaco(tmp_in.name, tmp_out.name)
-            with open(tmp_out.name, "rb") as fd_out:
-                ret = fd_out.read()
+
+        tmp_out = tempfile.NamedTemporaryFile()
+        tmp_out_name = tmp_out.name
+        tmp_out.close()
+
+        with tempfile.NamedTemporaryFile("wb", delete=False) as tmp_in:
+            tmp_in.write(hsaco)
+            amd.link_hsaco(tmp_in.name, tmp_out_name)
+            with open(tmp_out_name, "rb") as tmp_out:
+                ret = tmp_out.read()
+
         return ret
 
     def add_stages(self, stages, options, language):
