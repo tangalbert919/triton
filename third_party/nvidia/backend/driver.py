woct0rdho/triton-windows

Add paths of cl and CUDA before loading anything

third_party/nvidia/backend/driver.py
@@ -12,8 +12,9 @@ from triton.backends.driver import GPUDriver
 dirname = os.path.dirname(os.path.realpath(__file__))
 include_dir = [os.path.join(dirname, "include")]
 if os.name == "nt":
-    cuda_path = os.environ.get("CUDA_PATH")
-    include_dir += [f"{cuda_path}\\include"]
+    from triton.windows_utils import find_cuda
+    _, cuda_inc_dirs, _ = find_cuda()
+    include_dir += cuda_inc_dirs
 libdevice_dir = os.path.join(dirname, "lib")
 libraries = ['cuda']
 
@@ -21,7 +22,8 @@ libraries = ['cuda']
 @functools.lru_cache()
 def libcuda_dirs():
     if os.name == "nt":
-        return [os.path.join(cuda_path, "lib", "x64")]
+        _, _, cuda_lib_dirs = find_cuda()
+        return cuda_lib_dirs
 
     env_libcuda_path = os.getenv("TRITON_LIBCUDA_PATH")
     if env_libcuda_path:

tangalbert919/triton

third_party/nvidia/backend/driver.py
@@ -14,8 +14,9 @@ from triton.backends.driver import GPUDriver
 dirname = os.path.dirname(os.path.realpath(__file__))
 include_dirs = [os.path.join(dirname, "include")]
 if os.name == "nt":
-    cuda_path = os.environ.get("CUDA_PATH")
-    include_dirs += [f"{cuda_path}\\include"]
+    from triton.windows_utils import find_cuda
+    _, cuda_inc_dirs, _ = find_cuda()
+    include_dirs += cuda_inc_dirs
 libdevice_dir = os.path.join(dirname, "lib")
 libraries = ['cuda']
 PyCUtensorMap = None
@@ -25,6 +26,10 @@ PyCUtensorMap = None
 def libcuda_dirs():
     if env_libcuda_path := knobs.nvidia.libcuda_path:
         return [env_libcuda_path]
+
+    if os.name == "nt":
+        _, _, cuda_lib_dirs = find_cuda()
+        return cuda_lib_dirs
 
     libs = subprocess.check_output(["/sbin/ldconfig", "-p"]).decode(errors="ignore")
     # each line looks like the following: